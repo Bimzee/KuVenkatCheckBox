33170fb15697ff0a78547e32f99fc503648c4f91
bimal varghese <bimalvv2005@gmail.com>
2017-10-15 12:46:54 +0530

    implemented checkobox for using EditorForModel method

---
+++ KuVenkatCheckBox/Controllers/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Text;
+using KuVenkatCheckBox.Models;
+
+namespace KuVenkatCheckBox.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        [HttpGet]
+        public ActionResult Index()
+        {
+            MVCDataModel db = new MVCDataModel();
+
+            return View(db.Cities);
+        }
+        [HttpPost]
+        public string Index(IEnumerable<City> cities)
+        {
+            if (cities.Count(x => x.IsSelected) == 0)
+            {
+                return "please select city";
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder();
+                sb.Append("You selected : ");
+
+                foreach (var city in cities)
+                {
+                    if (city.IsSelected)
+                    {
+                        sb.Append(city.Name + ",");
+                    }
+                }
+                return sb.ToString();
+            }
+        }
+    }
+}
