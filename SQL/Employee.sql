f97f0bfb235a6df02b020ea0f3e27b8914b5db67
bimal varghese <bimalvv2005@gmail.com>
2017-10-29 01:28:24 +0530

    Not sure completed Chapter 43

---
+++ SQL/Employee.sql
@@ -0,0 +1,16 @@
+Create table Employee
+(
+ Id int primary key identity,
+ FullName nvarchar(100),
+ Gender nvarchar(10),
+ Age int,
+ HireDate DateTime,
+ EmailAddress nvarchar(100),
+ Salary int,
+ PersonalWebSite nvarchar(100)
+) 
+
+Insert into Employee values
+('John Smith', 'Male', 35, '2007-01-02 17:53:46.833', 'JohnSmith@pragimtech.com', 45000, 'http://www.pragimtech.com')
+Insert into Employee values
+('Mary Jane', NULL, 30, '2009-05-02 19:43:25.965', 'MaryJane@pragimtech.com', 35000, 'http://www.pragimtech.com')
